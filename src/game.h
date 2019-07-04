1d2853c2bb87abcae2531f4f4d30096e2f6a770d
usuari <aniol.sala.95@gmail.com>
2019-07-04 22:09:01 +0200

    Creating a window

---
+++ src/game.h
@@ -0,0 +1,4 @@
+
+// Basic definitions
+#define SCREEN_WIDTH 640.0
+#define SCREEN_HEIGHT 480.0
--- src/main.c
+++ src/main.c
@@ -1,12 +1,59 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include <SDL2/SDL.h>
 
+#include "game.h"
+
 int main(int argc, char* argv[])
 {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        fprintf(stderr, "Could not initialize sdl2: %s\n", SDL_GetError());
+        return EXIT_FAILURE;
+    }
 
-    printf("Hello world\n");
-    return 0;
+    SDL_Window* window = SDL_CreateWindow("Procedural",
+        100, 100,
+        SCREEN_WIDTH, SCREEN_HEIGHT,
+        SDL_WINDOW_SHOWN);
+
+    if (window == NULL) {
+        fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1,
+        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (renderer == NULL) {
+        SDL_DestroyWindow(window);
+        fprintf(stderr, "SDL_CreateRenderer Error: %s\n", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+
+    // Manage the window
+    SDL_Event e;
+    bool close_window = false;
+    while (close_window == false) {
+        while (SDL_PollEvent(&e)) {
+            switch (e.type) {
+            case SDL_QUIT:
+                close_window = 1;
+                break;
+
+                // More cases here
+
+            default: {
+            }
+            }
+        }
+    }
+
+    // Finish the window
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    printf("Program terminated with exit\n");
+    return EXIT_SUCCESS;
 }
